397482bf4cbfc7595d2f3b171556afb661e8e8c1
Paulo Godnho <paulo.silva@aquiris.com.br>
2022-04-17 01:34:00 -0300

    Typo fixes

--- Main.groovy
+++ Main.groovy
@@ -2,6 +2,6 @@ import Templates
 
 Templates templateCreator = new Templates(securityGroup: 'SecurityGroup', subnetId: 'MySubnet', iamProfile: 'MyProfile', zone: 'MyZone')
 
-Map windowsTemplate = templateCreator.windowsTemplate(ami: 'AmiValue', name: 'MyWindows', instanceType: 'c5.2xlarge', instanceCap '14')
+Map windowsTemplate = templateCreator.windowsTemplate(ami: 'AmiValue', name: 'MyWindows', instanceType: 'c5.2xlarge', instanceCap: '14')
 
 println(windowsTemplate)
